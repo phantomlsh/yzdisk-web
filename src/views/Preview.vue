110db3a5c900127f4f5ed5846840159a12b9bbed
Tina Ji <z5191511@ad.unsw.edu.au>
2022-09-11 22:54:03 +1000

    preview

---
+++ src/views/Preview.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const nid = route.params.nid, token = route.params.token
+const src = "https://s.yzzx.org/disk/#/card/" + nid + "?token" + token
+</script>
+
+<template>
+  <iframe :src="src"></iframe>
+</template>
